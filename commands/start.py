4a8ab2cef57096f53f67cd2fd06a50acd503dc47
Eugene Vereshchagin <veresha23@gmail.com>
2022-05-20 13:54:09 +0300

    i've got it!

--- commands/start.py
+++ commands/start.py
@@ -2,7 +2,6 @@ from telebot.types import Message
 from load_bot import bot
 
 
-# /start -> start
 @bot.message_handler(commands=['start'])
 def hello_message(message: Message):
     bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
